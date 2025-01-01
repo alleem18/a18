a774890313a5d5a148511bcaf821758abbfb6f20
“Aleemuddin <aleem2747@gmail.com>
2024-12-31 22:56:20 -0600

    updated link and increased gap between title and header

--- cleanfolio-main/src/portfolio.js
+++ cleanfolio-main/src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://thisisaleem.netlify.app',
+  homepage: 'https://aleemuddin.netlify.app/',
   title: '</AM>',
 }
 
